10aacfcbfcb030c89832f4c8345d8e9342d84712
Vitali Pisani <88674055+Vitali115@users.noreply.github.com>
2021-10-27 16:49:06 +0200

    updated // html + css //

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+/*Dati tre array contenenti:
+ - una lista ordinata di 5 immagini,
+ - una lista ordinata dei relativi 5 luoghi e
+ - una lista di 5 news,
+creare un carosello come nella foto allegata.
+Al click dell’utente sulle frecce verso l’alto o verso il basso, l’immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi
+ - titolo e
+ - testo.
+Allo stesso tempo nelle miniature l’immagine attiva dovrà apparire in evidenza rispetto alle altre.
+**Prima di partire a scrivere codice:**
+Non lasciamoci spaventare dalla complessità apparente dell’esercizio, ma analizziamo prima, come abbiamo fatto sempre, cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche giorno la sessione HTML e CSS, se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una **mezz’ora**, così da non perdere di vista il focus dell’esercizio.
+**Consigli del giorno:**
+1. costruiamo del carosello una versione statica contenente un’immagine grande con del testo ben posizionato e una miniatura. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come “template”.
+2. scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
+3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: “Quanti cicli servono?”
+Zip */
+
+
